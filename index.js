7224f49c69f03213caf3679f1a524ba384d1ceb7
Luke hwang <luke.hwang@buzzvil.com>
2019-05-24 05:55:42 +0900

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// calculator 객체 만들기.
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
